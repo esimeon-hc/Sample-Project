355f7d66cf32a1fba87e0bc03c48ad386d4c1403
admin <admin@TeamCity>
2018-06-19 20:17:52 +0200

    TeamCity change in '<Root project>' project: Versioned settings configuration updated

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,6 +1,9 @@
 import jetbrains.buildServer.configs.kotlin.v2018_1.*
+import jetbrains.buildServer.configs.kotlin.v2018_1.buildFeatures.swabra
+import jetbrains.buildServer.configs.kotlin.v2018_1.buildSteps.dotnetBuild
 import jetbrains.buildServer.configs.kotlin.v2018_1.buildSteps.maven
 import jetbrains.buildServer.configs.kotlin.v2018_1.triggers.vcs
+import jetbrains.buildServer.configs.kotlin.v2018_1.vcs.GitVcsRoot
 
 /*
 The settings script is an entry point for defining a TeamCity
@@ -27,11 +30,36 @@ To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
 version = "2018.1"
 
 project {
+    description = "Contains all other projects"
 
-    buildType(Build)
+    features {
+        feature {
+            id = "PROJECT_EXT_1"
+            type = "ReportTab"
+            param("startPage", "coverage.zip!index.html")
+            param("title", "Code Coverage")
+            param("type", "BuildReportTab")
+        }
+    }
+
+    cleanup {
+        preventDependencyCleanup = false
+    }
+
+    subProject(SampleProject)
+    subProject(SampleProject2)
 }
 
-object Build : BuildType({
+
+object SampleProject : Project({
+    name = "Sample Project"
+
+    buildType(SampleProject_Build)
+
+    subProject(SampleProject_Subproject1)
+})
+
+object SampleProject_Build : BuildType({
     name = "Build"
 
     vcs {
@@ -49,4 +77,78 @@ object Build : BuildType({
         vcs {
         }
     }
+
+    features {
+        swabra {
+        }
+    }
+})
+
+
+object SampleProject_Subproject1 : Project({
+    name = "Subproject1"
+
+    vcsRoot(SampleProject_Subproject1_HttpsGithubComJuliaAlexandrovaMyProjects1refsHeadsMast)
+
+    buildType(SampleProject_Subproject1_Subproject1Build)
+})
+
+object SampleProject_Subproject1_Subproject1Build : BuildType({
+    name = "Subproject1_Build"
+
+    vcs {
+        root(SampleProject_Subproject1_HttpsGithubComJuliaAlexandrovaMyProjects1refsHeadsMast)
+    }
+
+    steps {
+        dotnetBuild {
+            projects = "Solutions/CSharp.sln"
+        }
+    }
+
+    triggers {
+        vcs {
+        }
+    }
+})
+
+object SampleProject_Subproject1_HttpsGithubComJuliaAlexandrovaMyProjects1refsHeadsMast : GitVcsRoot({
+    name = "https://github.com/Julia-Alexandrova/My-Projects1#refs/heads/master"
+    url = "https://github.com/Julia-Alexandrova/My-Projects1"
+    param("teamcity:vcsResourceDiscovery:versionedSettingsRoot", "false")
+})
+
+
+object SampleProject2 : Project({
+    name = "Sample Project 2"
+
+    vcsRoot(SampleProject2_HttpsGithubComJuliaAlexandrovaJavaSampleRefsHeadsMaster)
+
+    buildType(SampleProject2_Build2)
+})
+
+object SampleProject2_Build2 : BuildType({
+    name = "Build 2"
+
+    vcs {
+        root(SampleProject2_HttpsGithubComJuliaAlexandrovaJavaSampleRefsHeadsMaster)
+    }
+
+    steps {
+        maven {
+            goals = "clean test"
+            mavenVersion = defaultProvidedVersion()
+        }
+    }
+
+    triggers {
+        vcs {
+        }
+    }
+})
+
+object SampleProject2_HttpsGithubComJuliaAlexandrovaJavaSampleRefsHeadsMaster : GitVcsRoot({
+    name = "https://github.com/Julia-Alexandrova/java_sample#refs/heads/master"
+    url = "https://github.com/Julia-Alexandrova/java_sample"
+    param("teamcity:vcsResourceDiscovery:versionedSettingsRoot", "false")
 })
